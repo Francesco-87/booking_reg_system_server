3e5bd6855c217bfaea6bf9f25eebf0c1ff274b86
Francesco-87 <francesco.ciccone@iubh.de>
2022-03-14 17:56:57 +0100

    testing deployment

--- config/allowedOrigins.js
+++ config/allowedOrigins.js
@@ -1,7 +1,8 @@
 const allowedOrigins =[
     'https://www.yoursite.com',
     'http://127.0.0.1:5500',
-    'http://localhost:3000'
+    'http://localhost:3000' ,
+    'http://bookingregsystemserver-env-2.eba-4rk3mr7m.us-east-1.elasticbeanstalk.com'
 ]
 
 module.exports = allowedOrigins;
