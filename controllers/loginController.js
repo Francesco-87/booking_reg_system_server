8262178a0c4303e5044e45577549e8866f7956f3
Francesco-87 <francesco.ciccone@iubh.de>
2022-03-14 19:15:02 +0100

    testing deployment

--- controllers/loginController.js
+++ controllers/loginController.js
@@ -17,90 +17,101 @@ const handleLogin = async (req, res) => {
 
 //checking for different users chefs/customer
             if (eMail.toString().includes("customer")) {
-                db.query("Select * from registrationsystem.customers where customer_eMail = ?;",
-                    [eMail],
-                    (err, result) => {
-                        if (err) {
-                            res.send({err: err})
-                        }
-                        //if a result is available, check for password
-                        if (result.length > 0) {
+                try{
+                    db.query("Select * from registrationsystem.customers where customer_eMail = ?;",
+                        [eMail],
+                        (err, result) => {
+                            if (err) {
+                                res.send({err: err})
+                            }
+                            //if a result is available, check for password
+                            if (result.length > 0) {
 
-                            bcrypt.compare(password, result[0].customer_password, (error, response) => {
-                                if (response) {
-                                    const idResult = result[0].idCustomer;
-                                    const accessToken = jwt.sign({idResult}, ACCESS_TOKEN_SECRET, {
-                                        expiresIn: '15m',
-                                    }); //setting the accessToken
-                                    const role = 1050;
+                                bcrypt.compare(password, result[0].customer_password, (error, response) => {
+                                    if (response) {
+                                        const idResult = result[0].idCustomer;
+                                        const accessToken = jwt.sign({idResult}, ACCESS_TOKEN_SECRET, {
+                                            expiresIn: '15m',
+                                        }); //setting the accessToken
+                                        const role = 1050;
 
-                                    const refreshToken = jwt.sign({idResult}, REFRESH_TOKEN_SECRET, {
-                                        expiresIn: '1d',
-                                    }); //setting the refreshToken
+                                        const refreshToken = jwt.sign({idResult}, REFRESH_TOKEN_SECRET, {
+                                            expiresIn: '1d',
+                                        }); //setting the refreshToken
 
-                                    res.cookie('jwt', refreshToken, {httpOnly:true, sameSite: 'None', secure: true, maxAge: 24 * 60 * 60 * 1000}); //setting the cookie
+                                        res.cookie('jwt', refreshToken, {httpOnly:true, sameSite: 'None', secure: true, maxAge: 24 * 60 * 60 * 1000}); //setting the cookie
 
-                                    res.json({auth: true, token: accessToken, roles: role, result: result}); //passing auth, token, and result to the frontend
+                                        res.json({auth: true, token: accessToken, roles: role, result: result}); //passing auth, token, and result to the frontend
 
-                                    // updating the database with a new refreshToken
-                                    db.query("UPDATE registrationsystem.customers SET customer_refreshToken = ? WHERE idCustomer = ?;", [refreshToken, idResult],
-                                        (err, result) => {
-                                            if (err) {
-                                                console.log(err);
-                                            }
-                                        })
-                                } else {
-                                    res.json({auth: false, message: "Wrong username/ password combination!"})
-                                }
-                            });
+                                        // updating the database with a new refreshToken
+                                        db.query("UPDATE registrationsystem.customers SET customer_refreshToken = ? WHERE idCustomer = ?;", [refreshToken, idResult],
+                                            (err, result) => {
+                                                if (err) {
+                                                    console.log(err);
+                                                }
+                                            })
+                                    } else {
+                                        res.json({auth: false, message: "Wrong username/ password combination!"})
+                                    }
+                                });
 
-                        } else {
-                            res.json({auth: false, message: "No User exists"})
-                        }
-                    });
+                            } else {
+                                res.json({auth: false, message: "No User exists"})
+                            }
+                        });
+                }catch (e) {
+                    console.log(e)
+                }
+
             }else if (eMail.toString().includes("chef")) {
-                db.query("Select * from registrationsystem.chefs where chef_eMail = ?",
-                    [eMail],
-                    (err, result) => {
-                        if (err) {
-                            res.send({err: err})
-                        }
-                        //if a result is available, check for password
-                        if (result.length > 0) {
 
-                            bcrypt.compare(password, result[0].chef_password, (error, response) => {
-                                if (response) {
-                                    const idResult = result[0].idChef;
-                                    const accessToken = jwt.sign({idResult}, ACCESS_TOKEN_SECRET, {
-                                        expiresIn: '15m',
-                                    });//setting the accessToken
-                                    const role = 2050;
-                                    const refreshToken = jwt.sign({idResult}, REFRESH_TOKEN_SECRET, {
-                                        expiresIn: '1d',
-                                    });//setting the refreshToken
+                try {
+                    db.query("Select * from registrationsystem.chefs where chef_eMail = ?",
+                        [eMail],
+                        (err, result) => {
+                            if (err) {
+                                res.send({err: err})
+                            }
+                            //if a result is available, check for password
+                            if (result.length > 0) {
 
-                                    res.cookie('jwt', refreshToken, {httpOnly:true, sameSite: 'None', secure: true, maxAge: 24 * 60 * 60 * 1000});//setting the cookie
+                                bcrypt.compare(password, result[0].chef_password, (error, response) => {
+                                    if (response) {
+                                        const idResult = result[0].idChef;
+                                        const accessToken = jwt.sign({idResult}, ACCESS_TOKEN_SECRET, {
+                                            expiresIn: '15m',
+                                        });//setting the accessToken
+                                        const role = 2050;
+                                        const refreshToken = jwt.sign({idResult}, REFRESH_TOKEN_SECRET, {
+                                            expiresIn: '1d',
+                                        });//setting the refreshToken
 
-                                    res.json({auth: true, token: accessToken, roles: role, result: result})//passing auth, token, and result to the frontend
+                                        res.cookie('jwt', refreshToken, {httpOnly:true, sameSite: 'None', secure: true, maxAge: 24 * 60 * 60 * 1000});//setting the cookie
 
-                                    // updating the database with a new refreshToken
-                                    db.query("UPDATE registrationsystem.chefs SET chef_refreshToken = ? WHERE idChef = ?;", [refreshToken, idResult],
-                                        (err, result) => {
-                                            if (err) {
-                                                console.log(err);
-                                            }
-                                        })
-                                } else {
-                                    res.json({auth: false, message: "Wrong username/ password combination!"})
-                                }
-                            });
+                                        res.json({auth: true, token: accessToken, roles: role, result: result})//passing auth, token, and result to the frontend
 
-                        } else {
-                            res.json({auth: false, message: "No User exists"})
+                                        // updating the database with a new refreshToken
+                                        db.query("UPDATE registrationsystem.chefs SET chef_refreshToken = ? WHERE idChef = ?;", [refreshToken, idResult],
+                                            (err, result) => {
+                                                if (err) {
+                                                    console.log(err);
+                                                }
+                                            })
+                                    } else {
+                                        res.json({auth: false, message: "Wrong username/ password combination!"})
+                                    }
+                                });
+
+                            } else {
+                                res.json({auth: false, message: "No User exists"})
 
 
-                        }
-                    });
+                            }
+                        });
+                }catch (e) {
+                    console.log(3)
+                }
+
             }
 }
 
