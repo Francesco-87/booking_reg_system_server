18f614d4915f0b3c7a20035be7b4688e3116804e
Francesco-87 <francesco.ciccone@iubh.de>
2022-03-17 21:30:39 +0100

    testing deployment

--- config/allowedOrigins.js
+++ config/allowedOrigins.js
@@ -2,7 +2,8 @@ const allowedOrigins =[
     'https://www.yoursite.com',
     'http://127.0.0.1:5500',
     'http://localhost:3000' ,
-    'http://bookingregsystemserver-env-2.eba-4rk3mr7m.us-east-1.elasticbeanstalk.com'
+    'http://bookingregsystemserver-env-2.eba-4rk3mr7m.us-east-1.elasticbeanstalk.com',
+    'https://master.dmby0on1mnprz.amplifyapp.com'
 ]
 
 module.exports = allowedOrigins;
