f827d1c8df6b34fbb0d65e595a3706baf4adeb15
Francesco-87 <francesco.ciccone@iubh.de>
2022-03-18 17:04:32 +0100

    testing deployment

--- config/allowedOrigins.js
+++ config/allowedOrigins.js
@@ -3,6 +3,7 @@ const allowedOrigins =[
     'http://127.0.0.1:5500',
     'http://localhost:3000' ,
     'http://bookingregsystemserver-env-2.eba-4rk3mr7m.us-east-1.elasticbeanstalk.com',
+   ' https://d3kh9fu01k6di6.cloudfront.net',
     'https://master.dmby0on1mnprz.amplifyapp.com'
 ]
 
